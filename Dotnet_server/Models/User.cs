02bda8926e3be37aa6dd31469c7031aea6425990
Taye Lemma <lemmataye@yahoo.com>
2025-05-02 19:21:27 -0500

    Dotnet project setup and user endpoint

--- Dotnet_server/Models/User.cs
+++ Dotnet_server/Models/User.cs
@@ -3,9 +3,17 @@ namespace Dotnet_server.Models;
 public partial class User
 {
     public int UserId { get; set; }
-    public string FirstName { get; set; } = string.Empty;
+    public string FirstName { get; set; } 
     public string LastName { get; set; } = "";
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
     public bool Active { get; set; }
+
+    public User()
+    {
+        if (FirstName is null)
+        {
+            FirstName = "";
+        }
+    }
     
 }
