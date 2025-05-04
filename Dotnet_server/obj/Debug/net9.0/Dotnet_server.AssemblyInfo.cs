645ecca5f1e6e42be2a7e4491706506b6f08a238
Taye Lemma <lemmataye@yahoo.com>
2025-05-04 03:51:54 -0500

    package

--- Dotnet_server/obj/Debug/net9.0/Dotnet_server.AssemblyInfo.cs
+++ Dotnet_server/obj/Debug/net9.0/Dotnet_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045af54fbffa4a7ade7e15674563882197c55feb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6843ffb06ba0dd021817c79ea34c5638d0586e81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
